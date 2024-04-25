4030ca5304f2d7536301236378d8c801a6eecc49
itsbasil <itsbasilgeorge@gmail.com>
2024-04-25 09:57:34 +0530

    commite

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xy_&^659mfyljh_me4o(bvx!88ht*_5^d4=a5r-)4!gr@gt_@e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'newsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'daXWbmryfCFaNeacfEphePmjGiDfRmBI',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '19003',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
